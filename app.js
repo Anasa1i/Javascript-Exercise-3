338880f6ee49b843538c2b0aa54c620ce1bd617e
Anas Ali <anasalimgmt@gmail.com>
2024-03-05 04:03:41 +0500

    UPDATE

---
+++ app.js
@@ -0,0 +1,166 @@
+
+// --------- CHAPTER NO 9 ----------
+//           QUESTION 1
+
+// var city = prompt("Enter Your City Name:");
+// if(city ==="Karachi"){
+//     document.write("<h1>Welcome to city of lights</h1>")
+// }
+
+//           QUESTION 2
+
+// var gender = prompt("Whats your Gender?") ;
+// if(gender === "male" ){
+//     document.write("<h1>Good Morning Sir.</h1>");
+// }
+// else if( gender === "female" ){
+//     document.write("<h1>Good Morning Maam.</h1>");
+// }
+// else {
+//     document.write("<h1>Better Luck Next time.</h1>");
+// }
+
+//           QUESTION 3
+
+// var color = prompt("color of road traffic signal");
+// if(color==="Red"){
+//     document.write("<h1>Must Stop</h1>");
+// }
+// else if(color==="Yellow"){
+//     document.write("<h1>Ready to Move</h1>");
+// }
+// else if(color==="Green"){
+//     document.write("<h1>Move Now</h1>");
+// }
+
+//           QUESTION 4
+
+// var fuel = prompt("remaining fuel in car 1 litre")
+// if( fuel < "0.25 litres"){
+//     document.write("“Please refill the fuel in your car")
+// }
+
+
+//           QUESTION 5
+
+// var a = 4;
+// if (++a === 5){
+//     alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (++b === 83){
+//     alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (++c === 13){
+//     alert("condition 1 is true");
+// }
+// if (c === 13){
+//     alert("condition 2 is true");
+// }
+// if (c++ >== 14){
+//     alert("condition 3 is true");
+// }
+// if(c === 14){
+//     alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost){
+//     alert("The cost equals" + " " + totalCost );
+// }
+
+
+// if (true){
+//    alert("True");
+// }
+// if (false){
+
+// }
+// alert("False");
+
+// if ("car" < "cat") {
+//     alert("cat is smaller than car");
+// }
+
+
+//           QUESTION 6
+
+
+    // var marks1 = +prompt("Enter marks obtained in Subject 1:");
+    // var marks2 = +prompt("Enter marks obtained in Subject 2:");
+    // var marks3 = +prompt("Enter marks obtained in Subject 3:");
+
+    // var totalMarks = prompt("Enter total marks:");
+
+    // var totalObtainedMarks = marks1 + marks2 + marks3;
+
+    // var percentage = totalObtainedMarks / totalMarks * 100;
+
+    // var grade, remarks;
+    // if (percentage >= 80) {
+    //     grade = 'A-one';
+    //     remarks = 'Excellent';
+    // } else if (percentage >= 70) {
+    //     grade = 'A';
+    //     remarks = 'Good';
+    // } else if (percentage >= 60) {
+    //     grade = 'B';
+    //     remarks = 'You need to improve';
+    // } else {
+    //     grade = 'Fail';
+    //     remarks = 'Sorry';
+    // }
+
+    // document.write("Total marks: " + totalMarks + "<br>");
+    // document.write("Marks Obtained: " + totalObtainedMarks + "<br>");
+    // document.write("Percentage: " + percentage(2) + "%" + "<br>");
+    // document.write("Grade: " + grade + "<br>");
+    // document.write("Remarks: " + remarks + "<br>");
+
+//   ------------------   SAME QUESTION ---------------
+
+
+//     var totalMarks1 = +prompt("Enter total marks of 1st subject:");
+// var totalMarks2 = +prompt("Enter total marks of 2nd subject:");
+// var totalMarks3 = +prompt("Enter total marks of 3rd subject:");
+// var totalMarks = totalMarks1+totalMarks2+totalMarks3;
+// var obtMarks1 = +prompt("Enter obtained marks of 1st subject:");
+// var obtMarks2 = +prompt("Enter obtained marks of 2nd subject:");
+// var obtMarks3 = +prompt("Enter obtained marks of 3rd subject:");
+// var totalObtMarks = obtMarks1 + obtMarks2 + obtMarks3;
+// var percentage1 = +(obtMarks1/totalMarks1)*100;
+// var percentage2 = +(obtMarks2/totalMarks2)*100;
+// var percentage3 = +(obtMarks3/totalMarks3)*100;
+// var totalpercent = totalObtMarks/totalMarks*100;
+// var grade;
+
+// if(percentage1 >= "80"){
+//     document.write("<br>Total Marks: " + totalMarks);
+//     document.write("<br>Marks Obtained: " + totalObtMarks);
+//     document.write("<br>Percentage: " + totalpercent + "%");
+//     document.write("<br>Grade: A-one");
+//     document.write("<br>Remarks: Excellent");
+// }else if(percentage1 >= "70"){
+//     document.write("<br>Total Marks: " + totalMarks);
+//     document.write("<br>Marks Obtained: " + totalObtMarks);
+//     document.write("<br>Percentage: " + totalpercent + "%");
+//     document.write("<br>Grade: A");
+//     document.write("<br>Remarks: Good");
+// }else if(percentage1 >= "60"){
+//     document.write("<br>Total Marks: " + totalMarks);
+//     document.write("<br>Marks Obtained: " + totalObtMarks);
+//     document.write("<br>Percentage: " + totalpercent + "%");
+//     document.write("<br>Grade: B");
+//     document.write("<br>Remarks: You need to improve");
+// }else if(percentage1 < "60"){
+//     document.write("<br>Total Marks: " + totalMarks);
+//     document.write("<br>Marks Obtained: " + totalObtMarks);
+//     document.write("<br>Percentage: " + totalpercent + "%");
+//     document.write("<br>Grade: Fail");
+//     document.write("<br>Remarks: Sorry");
+// }

eb88f2b09cbaa70415ee5726b780f56cba8bf046
Anas Ali <anasalimgmt@gmail.com>
2024-03-07 02:51:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-// --------- CHAPTER NO 9 ----------
+// --------- CHAPTER NO 9 to 11----------
 //           QUESTION 1
 
 // var city = prompt("Enter Your City Name:");
@@ -89,116 +89,225 @@
 
 
 //           QUESTION 6
+//  ------------------ want to learn formula ------------------
 
-
-    // var marks1 = +prompt("Enter marks obtained in Subject 1:");
-    // var marks2 = +prompt("Enter marks obtained in Subject 2:");
-    // var marks3 = +prompt("Enter marks obtained in Subject 3:");
-
-    // var totalMarks = prompt("Enter total marks:");
-
-    // var totalObtainedMarks = marks1 + marks2 + marks3;
-
-    // var percentage = totalObtainedMarks / totalMarks * 100;
-
-    // var grade, remarks;
-    // if (percentage >= 80) {
-    //     grade = 'A-one';
-    //     remarks = 'Excellent';
-    // } else if (percentage >= 70) {
-    //     grade = 'A';
-    //     remarks = 'Good';
-    // } else if (percentage >= 60) {
-    //     grade = 'B';
-    //     remarks = 'You need to improve';
-    // } else {
-    //     grade = 'Fail';
-    //     remarks = 'Sorry';
-    // }
-
-    // document.write("Total marks: " + totalMarks + "<br>");
-    // document.write("Marks Obtained: " + totalObtainedMarks + "<br>");
-    // document.write("Percentage: " + percentage(2) + "%" + "<br>");
-    // document.write("Grade: " + grade + "<br>");
-    // document.write("Remarks: " + remarks + "<br>");
-
-//   ------------------   SAME QUESTION ---------------
-
-
-//     var totalMarks1 = +prompt("Enter total marks of 1st subject:");
-// var totalMarks2 = +prompt("Enter total marks of 2nd subject:");
-// var totalMarks3 = +prompt("Enter total marks of 3rd subject:");
+// var totalMarks1 = +prompt("Enter total marks of first subject") ;
+// var totalMarks2 = +prompt("Enter total marks of Second subject") ;
+// var totalMarks3 = +prompt("Enter total marks of Third subject") ;
 // var totalMarks = totalMarks1+totalMarks2+totalMarks3;
-// var obtMarks1 = +prompt("Enter obtained marks of 1st subject:");
-// var obtMarks2 = +prompt("Enter obtained marks of 2nd subject:");
-// var obtMarks3 = +prompt("Enter obtained marks of 3rd subject:");
-// var totalObtMarks = obtMarks1 + obtMarks2 + obtMarks3;
-// var percentage1 = +(obtMarks1/totalMarks1)*100;
-// var percentage2 = +(obtMarks2/totalMarks2)*100;
-// var percentage3 = +(obtMarks3/totalMarks3)*100;
-// var totalpercent = totalObtMarks/totalMarks*100;
+// var markObt1 = +prompt("Enter Obtained marks of First subject")
+// var markObt2 = +prompt("Enter Obtained marks of Second subject")
+// var markObt3 = +prompt("Enter Obtained marks of Third subject")
+// var obtainedMarks = markObt1+markObt2+markObt3;
+// var percentage1 = +(markObt1/totalMarks1)*100 ;
+// var percentage2 = +(markObt2/totalMarks2)*100 ;
+// var percentage3 = +(markObt3/totalMarks3)*100 ;
+// var totalPercent = obtainedMarks/totalMarks*100 ;
 // var grade;
-
 // if(percentage1 >= "80"){
-//     document.write("<br>Total Marks: " + totalMarks);
-//     document.write("<br>Marks Obtained: " + totalObtMarks);
-//     document.write("<br>Percentage: " + totalpercent + "%");
-//     document.write("<br>Grade: A-one");
-//     document.write("<br>Remarks: Excellent");
-// }else if(percentage1 >= "70"){
-//     document.write("<br>Total Marks: " + totalMarks);
-//     document.write("<br>Marks Obtained: " + totalObtMarks);
-//     document.write("<br>Percentage: " + totalpercent + "%");
-//     document.write("<br>Grade: A");
-//     document.write("<br>Remarks: Good");
-// }else if(percentage1 >= "60"){
-//     document.write("<br>Total Marks: " + totalMarks);
-//     document.write("<br>Marks Obtained: " + totalObtMarks);
-//     document.write("<br>Percentage: " + totalpercent + "%");
-//     document.write("<br>Grade: B");
-//     document.write("<br>Remarks: You need to improve");
-// }else if(percentage1 < "60"){
-//     document.write("<br>Total Marks: " + totalMarks);
-//     document.write("<br>Marks Obtained: " + totalObtMarks);
-//     document.write("<br>Percentage: " + totalpercent + "%");
-//     document.write("<br>Grade: Fail");
-//     document.write("<br>Remarks: Sorry");
+//     document.write("<br>Total Marks: " + totalMarks )
+//     document.write("<br>Marks Obtained: " + obtainedMarks )
+//     document.write("<br>Percentage: " + totalPercent + "%"  )
+//     document.write("<br>Grade: A-one " ) 
+//     document.write("<br>Remarks: Excellent " )
+// }
+// else if (percentage1 >= "70"){
+//     document.write("<br>Total Marks: " + totalMarks )
+//     document.write("<br>Marks Obtained: " + obtainedMarks )
+//     document.write("<br>Percentage: " + totalPercent + "%"  )
+//     document.write("<br>Grade: A " ) 
+//     document.write("<br>Remarks: Good " )
+// }
+// else if (percentage1 >= "60"){
+//     document.write("<br>Total Marks: " + totalMarks )
+//     document.write("<br>Marks Obtained: " + obtainedMarks )
+//     document.write("<br>Percentage: " + totalPercent + "%"  )
+//     document.write("<br>Grade: B " ) 
+//     document.write("<br>Remarks: You Need to improve " )
+// }
+// else if(percentage1 < "60"){
+//     document.write("<br>Total Marks: " + totalMarks )
+//     document.write("<br>Marks Obtained: " + obtainedMarks )
+//     document.write("<br>Percentage: " + totalpercent + "%"  ) ;
+//     document.write("<br>Grade: Fail " ) ;
+//     document.write("<br>Remarks: sorry " ) ;
 // }
 
-// ------------------- QUESTION 6 SAME ---------------------
 
-// // markSheet.js
+//           QUESTION 7
 
-// // Marks prapt karne ke liye user se input lena
-// var subject1Marks = parseFloat(prompt("Enter marks obtained in Subject 1:"));
-// var subject2Marks = parseFloat(prompt("Enter marks obtained in Subject 2:"));
-// var subject3Marks = parseFloat(prompt("Enter marks obtained in Subject 3:"));
-// var totalMarks = parseFloat(prompt("Enter total marks:"));
+//  var secretNumber = (3) ;
+//  var userGuess = prompt("guess the secret number, between 1 to 10.")
 
-// // Percentage ka calculation
-// var totalObtainedMarks = subject1Marks + subject2Marks + subject3Marks;
-// var percentage = (totalObtainedMarks / totalMarks) * 100;
+//  if(userGuess == secretNumber ){
+//     document.write("Bingo! Correct answer")
+//  }
 
-// // Grade aur Remarks ka calculation
-// var grade, remarks;
-// if (percentage >= 80) {
-//     grade = 'A-one';
-//     remarks = 'Excellent';
-// } else if (percentage >= 70) {
-//     grade = 'A';
-//     remarks = 'Good';
-// } else if (percentage >= 60) {
-//     grade = 'B';
-//     remarks = 'You need to improve';
-// } else {
-//     grade = 'Fail';
-//     remarks = 'Sorry';
+//  else if (userGuess == "2" ){
+//     document.write("Close enough to the correct answer")
+//  }
+
+//  else if (userGuess == "4" ){
+//     document.write("Close enough to the correct answer")
+//  }
+
+//  else{
+//     document.write("sorry! try again letter...")
+//  }
+
+//           QUESTION 8
+
+// var userNumber = prompt("Write a number: ")
+// if (userNumber % 3 == 0) {
+//     document.write( userNumber + "is divisible by 3.")
+// }
+// else {
+//     document.write( userNumber + "is not divisible by 3, please try again later.")
 // }
 
-// // Result ko display karna
-// console.log("Mark Sheet");
-// console.log("Total marks: " + totalMarks);
-// console.log("Marks Obtained: " + totalObtainedMarks);
-// console.log("Percentage: " + percentage.toFixed(2) + "%");
-// console.log("Grade: " + grade);
-// console.log("Remarks: " + remarks);
+//           QUESTION 9
+
+// var num = prompt("Enter a Number")
+// if(num % 2 == 0){
+//     document.write( num + " is an Even number")
+// }
+// else {
+//     document.write( num + " is an Odd number")
+// }
+
+//           QUESTION 10
+
+// var tem = prompt("enter a temperature number")
+
+// if(tem > 40){
+//     document.write("It is too hot outside.")
+// }
+// else if(tem > 30){
+//     document.write("The Weather today is Normal.")
+// }
+// else if(tem > 20){
+//     document.write("Today’s Weather is cool.")
+// }
+// else if(tem > 10){
+//     document.write("OMG! Today’s weather is so Cool.")
+// }
+
+//           QUESTION 11
+
+// var numOne = +prompt("enter a number One")
+// var cal = prompt()
+// var numTwo = +prompt("enter a number Two")
+
+// if( cal == "-"){
+//     document.write(numOne - numTwo)
+// }
+
+// else if( cal == "*"){
+//     document.write(numOne * numTwo)
+// }
+
+// else if ( cal == "+") {
+//     document.write(numOne + numTwo)
+// }
+
+// else if ( cal == "/") {
+//     document.write(numOne / numTwo)
+// }
+
+// else {
+//     document.write("insert a number")
+// }
+
+
+// --------- CHAPTER NO 12 to 13 ----------
+
+//                   Question 1
+
+
+
+
+
+
+
+
+
+                    //  EMPTY
+
+
+
+
+
+
+
+
+
+
+
+
+
+//           QUESTION 2 
+
+// var integer1 = prompt("Enter a integer number one");
+// var integer2 = prompt("Enter a integer number two");
+
+// if (integer1 === integer2) {
+//     document.write("The two integers are equal.");
+// }
+// else if (integer1 > integer2) {
+//     document.write("The larger integer is: " + integer1);
+// }
+// else {
+//     document.write("The larger integer is: " + integer2);
+// }
+
+
+//           QUESTION 3
+
+// var num1 = prompt("Enter a number")
+
+// if (num1 > 0 ){
+//     document.write("the number is positive")
+// }
+
+// else if(num1 < 0 ){
+//     document.write("the number is Negative")
+// }
+
+// else {
+//     document.write("the number is Zero")
+// }
+
+//           QUESTION 4
+
+
+
+
+
+
+
+//           QUESTION 5
+
+// var password = "sippinstore"
+// var entry = prompt("Enter Your Password:")
+
+// if (entry == password){
+//     document.write("“Correct! The password you entered matches the original password")
+// }
+// else {
+//     document.write("Incorrect password")
+// }
+
+//           QUESTION 6
+
+// var greeting; 
+// var hour = 13; 
+// if (hour < "18") { 
+// document.write( "Good day" )
+// }
+// else {
+// document.write("Good evening" )
+// } 
+
+//           QUESTION 7
+
+
